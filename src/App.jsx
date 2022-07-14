be8117f1f6e7b7839d2e5ffbe7638151ee610266
Deviego <diegodomingues266@gmail.com>
2022-07-14 11:51:59 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Calculator from './components/calculator'
+import './App.css'
+
+function App() {
+ 
+  return (
+   <Calculator></Calculator>
+  )
+}
+
+export default App
---
+++ src/components/calculator.jsx
@@ -0,0 +1,16 @@
+import './calculator.css'
+import'@emotion/react'
+import '@emotion/styled'
+import'@mui/material'
+
+export default function Calculator(){
+    return(
+        <div >
+            <Container>
+            <h1>Calculator ReactJs</h1>
+            <Box n={12}/>
+        </Container>
+        </div>
+        
+    )
+}
